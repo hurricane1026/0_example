01ee90fbfb6e8555a832b1f1813ee45bb02ab0a6
hurricane1026 <hurricane1026@gmail.com>
2018-09-10 14:03:11 +0800

    add the first example

---
+++ object.py
@@ -0,0 +1,31 @@
+from triones.Objects import TriObject, sales_contacts
+
+#from triones.Tenant import TenantManager
+
+from triones.ACL import TokenManager
+
+from triones import Console
+
+token_path = ""
+
+
+dev_token = TokenManager(token_path)
+
+def CreateLeads():
+    meta_of_leads = {
+            "name" : "string",
+            "information" : "string",
+            "value" : "integer"
+            }
+    leads_obj = TriObject("Leads", prefix = "")
+    leads_obj.add_fields(meta_of_leads)
+    # by now, add 3 fields
+
+    leads_obj.fields["value"].add_relation(sales_contacts.fields["value"], category="lookup")
+
+    return leads_obj
+
+console = Console(dev_token)
+
+tenant = console.get_tenant("example_company")
+
